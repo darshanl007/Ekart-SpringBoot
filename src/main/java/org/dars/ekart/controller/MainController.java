5eb38784573b9c770c4a162846e28b96c40d7ee2
Darshan L <darshanl1254@gmail.com>
2024-12-25 02:17:33 +0530

    simple load home page

---
+++ src/main/java/org/dars/ekart/controller/MainController.java
@@ -0,0 +1,13 @@
+package org.dars.ekart.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+	@GetMapping("/")
+	public String loadHome() {
+		return "home.html";
+	}
+}
